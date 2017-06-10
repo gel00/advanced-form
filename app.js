3fe9c7ec4247c8b63d3af51da80852e894fb7013
gel00 <gellaigabor@gmail.com>
2017-06-10 10:50:56 +0200

    adding comments stage 1

--- app.js
+++ app.js
@@ -1,138 +1,242 @@
+/* JSON data from server, that contains
+ * question cards a list of input fields
+ */
+
 var rawCardList = [
+  {
+    "name": "Contact",
+    "question": "How can we contact you?",
+    "icon": "images/contact.png",
+    "inputList": [
       {
-        name : 'Contact',
-        question : "How can we contact you?",
-        icon : "images/contact.png",
-        inputList : [
-           {
-             label : "email",
-             attrList : {
-               type : "email", id : "cf-email", required : true, autocomplete : "email", placeholder : "e-mail address"
-             }
-           },
-          {
-             label : "tel",
-             attrList : {
-               type : "tel", id : "cf-tel", required : true, autocomplete : "tel", placeholder : "telephone number"
-             }
-           }
-          ]
-        }, {
-          name : "Delivery",
-          question : "Which address should we send your order to?",
-          icon : "images/delivery.png",
-          inputList : [
-            {
-              label : "name",
-              attrList : {
-                type : "text", id : "cf-name", required : true, autocomplete : "name", placeholder : "full name"
-              }
-            }, {
-              label : "address",
-              attrList : {
-                type : "text", id : "cf-address", required : true, autocomplete : "street-address", placeholder : "street address"
-              }
-            }, {
-              label : "postal code",
-              attrList : {
-                class: "cf-25", type : "number", id : "cf-postal", required : true, autocomplete : "postal-code", placeholder : "zipcode", min : "1007", max : "9999", maxlength : 4
-              }
-            }, {
-              label : "city",
-              attrList : {
-                class: "cf-75", type : "text", id : "cf-city", required : true, autocomplete : "on", placeholder : "city"
-              }
-            }
-          ]
-        }, {
-          name : "Billing",
-          question : "Which address should we use for billing?",
-          icon : "images/billing.png",
-					controller: 2, //first 2 inputfield
-          inputList : [
-            {
-              label: "same address",
-              attrList : {
-                class: "cf-50", type : "radio", id : "cf-b-same", name : "billingAddress", value : "same", checked : true
-              }
-            }, {
-              label: "different address",
-              attrList : {
-                class: "cf-50", type : "radio", id : "cf-b-different", name : "billingAddress", value : "different"
-              }
-            }, {
-              label : "name",
-              attrList : {
-                type : "text", id : "cf-name-b", required : true, autocomplete : "name", placeholder : "full name", disabled : true
-              }
-            }, {
-              label : "address",
-              attrList : {
-                type : "text", id : "cf-address-b", required : true, autocomplete : "street-address", placeholder : "street address", disabled : true
-              }
-            }, {
-              label : "postal code",
-              attrList : {
-                class: "cf-25", type : "number", id : "cf-postal-b", required : true, autocomplete : "postal-code", placeholder : "zipcode", min : "1007", max : "9999", maxlength : 4, disabled : true
-              }
-            }, {
-              label : "city",
-              attrList : {
-                class: "cf-75", type : "text", id : "cf-city-b", required : true, autocomplete : "on", placeholder : "city", disabled : true
-              }
-            }
-          ]
-        }, {
-          name : "Payment",
-          question : "How would you like to pay?",
-          icon : "images/payment.png",
-					controller: 2, //first 2 inputfield
-          inputList : [
-            {
-              label: "C.O.D.",
-              attrList : {
-                class: "cf-50", type : "radio", id : "cf-p-same", name : "paymentmethod", value : "c.o.d."
-              }
-            }, {
-              label: "Credit Card",
-              attrList : {
-                class: "cf-50", type : "radio", id : "cf-b-different", name : "paymentmethod", value : "credit card",  checked : true
-              }
-            }, {
-              label : "Name on card",
-              attrList : {
-                type : "text", id : "cf-cc-name", required : true, autocomplete : "cc-name", placeholder : "full name"
-              }
-            }, {
-              label : "Card Number",
-              attrList : {
-                type : "text", id : "cf-cc-number", required : true, autocomplete : "cc-number", placeholder : "credit card number", pattern : "[0-9-]{12,20}"
-              }
-            }, {
-              label : "CVC",
-              attrList : {
-                class: "cf-25", type : "text", id : "cf-cc-cvc", required : true, autocomplete : "cc-csc", placeholder : "cvc code", pattern: "[0-9]{3,4}"
-              }
-            }, {
-              label : "expiration date",
-              attrList : {
-                class: "cf-75", type : "text", id : "cf-cc-exp", required : true, autocomplete : "cc-exp", placeholder : "MM-YYYY", pattern : "^[0-9]{2}-20[12][0-9]$"
-              }
-            }
-          ]
+        "label": "email",
+        "attrList": {
+          "type": "email",
+          "id": "cf-email",
+          "required": true,
+          "autocomplete": "email",
+          "placeholder": "e-mail address"
         }
-    ];
+      },
+      {
+        "label": "tel",
+        "attrList": {
+          "type": "tel",
+          "id": "cf-tel",
+          "required": true,
+          "autocomplete": "tel",
+          "placeholder": "telephone number"
+        }
+      }
+    ]
+  },
+  {
+    "name": "Delivery",
+    "question": "Which address should we send your order to?",
+    "icon": "images/delivery.png",
+    "inputList": [
+      {
+        "label": "name",
+        "attrList": {
+          "type": "text",
+          "id": "cf-name",
+          "required": true,
+          "autocomplete": "name",
+          "placeholder": "full name"
+        }
+      },
+      {
+        "label": "address",
+        "attrList": {
+          "type": "text",
+          "id": "cf-address",
+          "required": true,
+          "autocomplete": "street-address",
+          "placeholder": "street address"
+        }
+      },
+      {
+        "label": "postal code",
+        "attrList": {
+          "class": "cf-25",
+          "type": "number",
+          "id": "cf-postal",
+          "required": true,
+          "autocomplete": "postal-code",
+          "placeholder": "zipcode",
+          "min": "1007",
+          "max": "9999",
+          "maxlength": 4
+        }
+      },
+      {
+        "label": "city",
+        "attrList": {
+          "class": "cf-75",
+          "type": "text",
+          "id": "cf-city",
+          "required": true,
+          "autocomplete": "on",
+          "placeholder": "city"
+        }
+      }
+    ]
+  },
+  {
+    "name": "Billing",
+    "question": "Which address should we use for billing?",
+    "icon": "images/billing.png",
+    "controller": 2,
+    "inputList": [
+      {
+        "label": "same address",
+        "attrList": {
+          "class": "cf-50",
+          "type": "radio",
+          "id": "cf-b-same",
+          "name": "billingAddress",
+          "value": "same",
+          "checked": true
+        }
+      },
+      {
+        "label": "different address",
+        "attrList": {
+          "class": "cf-50",
+          "type": "radio",
+          "id": "cf-b-different",
+          "name": "billingAddress",
+          "value": "different"
+        }
+      },
+      {
+        "label": "name",
+        "attrList": {
+          "type": "text",
+          "id": "cf-name-b",
+          "required": true,
+          "autocomplete": "name",
+          "placeholder": "full name",
+          "disabled": true
+        }
+      },
+      {
+        "label": "address",
+        "attrList": {
+          "type": "text",
+          "id": "cf-address-b",
+          "required": true,
+          "autocomplete": "street-address",
+          "placeholder": "street address",
+          "disabled": true
+        }
+      },
+      {
+        "label": "postal code",
+        "attrList": {
+          "class": "cf-25",
+          "type": "number",
+          "id": "cf-postal-b",
+          "required": true,
+          "autocomplete": "postal-code",
+          "placeholder": "zipcode",
+          "min": "1007",
+          "max": "9999",
+          "maxlength": 4,
+          "disabled": true
+        }
+      },
+      {
+        "label": "city",
+        "attrList": {
+          "class": "cf-75",
+          "type": "text",
+          "id": "cf-city-b",
+          "required": true,
+          "autocomplete": "on",
+          "placeholder": "city",
+          "disabled": true
+        }
+      }
+    ]
+  },
+  {
+    "name": "Payment",
+    "question": "How would you like to pay?",
+    "icon": "images/payment.png",
+    "controller": 2,
+    "inputList": [
+      {
+        "label": "C.O.D.",
+        "attrList": {
+          "class": "cf-50",
+          "type": "radio",
+          "id": "cf-p-same",
+          "name": "paymentmethod",
+          "value": "c.o.d."
+        }
+      },
+      {
+        "label": "Credit Card",
+        "attrList": {
+          "class": "cf-50",
+          "type": "radio",
+          "id": "cf-b-different",
+          "name": "paymentmethod",
+          "value": "credit card",
+          "checked": true
+        }
+      },
+      {
+        "label": "Name on card",
+        "attrList": {
+          "type": "text",
+          "id": "cf-cc-name",
+          "required": true,
+          "autocomplete": "cc-name",
+          "placeholder": "full name"
+        }
+      },
+      {
+        "label": "Card Number",
+        "attrList": {
+          "type": "text",
+          "id": "cf-cc-number",
+          "required": true,
+          "autocomplete": "cc-number",
+          "placeholder": "credit card number",
+          "pattern": "[0-9-]{12,20}"
+        }
+      },
+      {
+        "label": "CVC",
+        "attrList": {
+          "class": "cf-25",
+          "type": "text",
+          "id": "cf-cc-cvc",
+          "required": true,
+          "autocomplete": "cc-csc",
+          "placeholder": "cvc code",
+          "pattern": "[0-9]{3,4}"
+        }
+      },
+      {
+        "label": "expiration date",
+        "attrList": {
+          "class": "cf-75",
+          "type": "text",
+          "id": "cf-cc-exp",
+          "required": true,
+          "autocomplete": "cc-exp",
+          "placeholder": "MM-YYYY",
+          "pattern": "^[0-9]{2}-20[12][0-9]$"
+        }
+      }
+    ]
+  }
+];
 
-/*-----view---------*/
-var question = document.getElementById("cf-question");
-var cardIcon = document.getElementById("cf-icon");
-var inputSection = document.getElementById("cf-body");
-var progressBar = document.getElementById("cf-progressbar");
-
-var cardList = [];
-var activeCard = 0;
-
-
+/*-----------Card-Class-------------*/
 function Card(obj) {
   var self = this;
   this.name = obj.name;
@@ -261,6 +365,8 @@ function loadNextCard(num) {
 		.createButton();
 }
 
+/*------------Input-Class--------------*/
+
 function Input(obj) {
   var self = this
   this.label = obj.label;
@@ -374,8 +480,22 @@ function createProgressBar() {
   });
 }
 
+/*-----------view-------------*/
 
+// Selectors for template
+var question = document.getElementById("cf-question");
+var cardIcon = document.getElementById("cf-icon");
+var inputSection = document.getElementById("cf-body");
+var progressBar = document.getElementById("cf-progressbar");
 
+var cardList = [];  //array of question cards
+var activeCard = 0; //index of initial question card
+
+/* Tis few lines construct every card with
+ * every inputs. After the first has been created
+ * and before constructing the rest of the cards
+ * it renders the first one.
+ */
 var card0 = new Card(rawCardList[0]).show().createNext();
 createProgressBar();
 var card1 = new Card(rawCardList[1]);
