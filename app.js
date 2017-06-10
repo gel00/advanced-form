5d6d5c5e0a9476e48d9e7401da3511cb6119fdc6
gel00 <gellaigabor@gmail.com>
2017-06-10 10:54:58 +0200

    beautifier JS

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 /* JSON data from server, that contains
  * question cards a list of input fields
  */
-
-var rawCardList = [
-  {
+var rawCardList = [{
     "name": "Contact",
     "question": "How can we contact you?",
     "icon": "images/contact.png",
-    "inputList": [
-      {
+    "inputList": [{
         "label": "email",
         "attrList": {
           "type": "email",
@@ -34,8 +31,7 @@ var rawCardList = [
     "name": "Delivery",
     "question": "Which address should we send your order to?",
     "icon": "images/delivery.png",
-    "inputList": [
-      {
+    "inputList": [{
         "label": "name",
         "attrList": {
           "type": "text",
@@ -87,8 +83,7 @@ var rawCardList = [
     "question": "Which address should we use for billing?",
     "icon": "images/billing.png",
     "controller": 2,
-    "inputList": [
-      {
+    "inputList": [{
         "label": "same address",
         "attrList": {
           "class": "cf-50",
@@ -165,8 +160,7 @@ var rawCardList = [
     "question": "How would you like to pay?",
     "icon": "images/payment.png",
     "controller": 2,
-    "inputList": [
-      {
+    "inputList": [{
         "label": "C.O.D.",
         "attrList": {
           "class": "cf-50",
@@ -240,12 +234,12 @@ var rawCardList = [
 function Card(obj) {
   var self = this;
   this.name = obj.name;
-	this.controller = obj.controller;
+  this.controller = obj.controller;
   this.question = obj.question;
   this.icon = obj.icon;
   this.inputList = [];
   this.index = cardList.length;
-  obj.inputList.forEach(function(input){
+  obj.inputList.forEach(function(input) {
     self.inputList.push(new Input(input));
   });
   cardList.push(this);
@@ -256,7 +250,7 @@ Card.prototype.show = function() {
   cardIcon.setAttribute("src", this.icon);
   cardIcon.setAttribute("alt", this.name);
   inputSection.innerHTML = "";
-  this.inputList.forEach(function(input){
+  this.inputList.forEach(function(input) {
     if (input.elem.type == "radio") {
       input.createLabel().appendChild(input.elem);
     } else {
@@ -264,53 +258,53 @@ Card.prototype.show = function() {
     }
     input.loadValue();
   });
-	return this;
+  return this;
 };
 
 Card.prototype.next = function() {
   this.validate();
-  if(this.valid) {
+  if (this.valid) {
     loadNextCard();
   }
   return this;
 };
 
 Card.prototype.createNext = function() {
-	var self = this;
-	var next = document.createElement("button");
-	next.textContent = "Next";
+  var self = this;
+  var next = document.createElement("button");
+  next.textContent = "Next";
   next.id = "cf-next";
   inputSection.appendChild(next);
-  next.addEventListener("click", function(){
-		self.next();
-  },false);
-	return this;
+  next.addEventListener("click", function() {
+    self.next();
+  }, false);
+  return this;
 };
-Card.prototype.createSubmit = function(){
+Card.prototype.createSubmit = function() {
   var self = this;
-	var submit = document.createElement("input");
-	submit.setAttribute("type", "submit");
-	submit.value = "Submit";
-	inputSection.appendChild(submit);
-  submit.addEventListener("click", function(){
-		self.validate();
+  var submit = document.createElement("input");
+  submit.setAttribute("type", "submit");
+  submit.value = "Submit";
+  inputSection.appendChild(submit);
+  submit.addEventListener("click", function() {
+    self.validate();
     if (self.valid) {
       alert("Your order has been sent.");
     }
-  },false);
-	return this;
+  }, false);
+  return this;
 };
 
 Card.prototype.createButton = function() {
-	cardList.length - 1 == activeCard ?
-		this.createSubmit() :
-		this.createNext();
+  cardList.length - 1 == activeCard ?
+    this.createSubmit() :
+    this.createNext();
 };
 
 Card.prototype.validate = function() {
-	var inputList = this.filterInput();
-	var result, i, l=this.inputList.length;
-	for (i = 0; i < l; i++) {
+  var inputList = this.filterInput();
+  var result, i, l = this.inputList.length;
+  for (i = 0; i < l; i++) {
     result = this.inputList[i].validate();
     if (!result) {
       break;
@@ -320,49 +314,49 @@ Card.prototype.validate = function() {
   return this;
 };
 
-Card.prototype.filterInput = function(){
-	var num = this.controller || 0;
-	return this.inputList.slice(num);
+Card.prototype.filterInput = function() {
+  var num = this.controller || 0;
+  return this.inputList.slice(num);
 };
 
-Card.prototype.setDisabled = function(bool){
-	this.filterInput().forEach(function(input){
-		input.elem.disabled = bool;
-	});
-	return this;
+Card.prototype.setDisabled = function(bool) {
+  this.filterInput().forEach(function(input) {
+    input.elem.disabled = bool;
+  });
+  return this;
 };
 
-Card.prototype.copyCardTo = function(destination){
-	dFiltered = destination.filterInput();
-	this.filterInput().forEach(function(input, i){
+Card.prototype.copyCardTo = function(destination) {
+  dFiltered = destination.filterInput();
+  this.filterInput().forEach(function(input, i) {
     input.copyValue(dFiltered[i]).setPair(dFiltered[i]);
-	});
-	return this;
+  });
+  return this;
 };
 
 Card.prototype.saveValues = function() {
-	this.inputList.forEach(function(input){
-		input.value = input.elem.value;
-	});
-	return this;
+  this.inputList.forEach(function(input) {
+    input.value = input.elem.value;
+  });
+  return this;
 };
 Card.prototype.loadValues = function() {
-	this.inputList.forEach(function(input){
-		input.elem.value = input.value || "";
-	});
-	return this;
+  this.inputList.forEach(function(input) {
+    input.elem.value = input.value || "";
+  });
+  return this;
 };
 
 function loadNextCard(num) {
-  num = typeof num != "undefined"? num : activeCard + 1;
+  num = typeof num != "undefined" ? num : activeCard + 1;
   var mileStones = progressBar.getElementsByTagName("span");
   mileStones.item(activeCard).style.background = "#828282";
-	cardList[activeCard].saveValues();
+  cardList[activeCard].saveValues();
   activeCard = num;
   mileStones.item(activeCard).style.background = "#3784e1";
   cardList[activeCard]
-		.show()
-		.createButton();
+    .show()
+    .createButton();
 }
 
 /*------------Input-Class--------------*/
@@ -371,40 +365,40 @@ function Input(obj) {
   var self = this
   this.label = obj.label;
   this.elem = document.createElement("input");
-  Object.keys(obj.attrList).forEach(function(attr){
+  Object.keys(obj.attrList).forEach(function(attr) {
     var value = obj.attrList[attr];
-    self.elem.setAttribute(attr,value);
-		if (attr == "value") {
-			this.value = value;
-		}
+    self.elem.setAttribute(attr, value);
+    if (attr == "value") {
+      this.value = value;
+    }
   });
   this.pattern = this.getPattern();
-  this.elem.addEventListener("focusout",function(){
+  this.elem.addEventListener("focusout", function() {
     self.validate();
-  },false)
+  }, false)
 }
 
 Input.prototype.saveValue = function() {
   this.value = this.elem.value;
-	return this;
+  return this;
 };
 
 Input.prototype.loadValue = function() {
   this.elem.value = this.pair ? this.pair.value : this.value || "";
-	return this;
+  return this;
 };
 
 Input.prototype.createLabel = function() {
-	var label = document.createElement("label");
-  label.setAttribute("for",this.elem.id);
+  var label = document.createElement("label");
+  label.setAttribute("for", this.elem.id);
   label.textContent = this.label;
   inputSection.appendChild(label);
-	return label;
+  return label;
 };
 
 Input.prototype.copyValue = function(destination) {
   destination.elem.value = this.value || "";
-	return this;
+  return this;
 };
 
 Input.prototype.setPair = function(destination) {
@@ -418,34 +412,34 @@ Input.prototype.removePair = function() {
 }
 
 Input.prototype.getPattern = function() {
-	var pattern = this.elem.getAttribute('pattern');
-	if (!pattern) {
-		switch(this.elem.getAttribute('type')) {
+  var pattern = this.elem.getAttribute('pattern');
+  if (!pattern) {
+    switch (this.elem.getAttribute('type')) {
 
-			case "email" :
-				pattern = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/;
-				break;
-			case "tel" :
-				pattern = /(\+3|0)6\({0,1}\d{2}\){0,1}[- ]{0,1}\d{3}[- ]{0,1}\d{3,4}/;
-				break;
-			default :
-				pattern = /^[a-z0-9]{2,}[ ,.'-]*[a-z0-9]+$/i;
-													}
-	}//if
-	return pattern;
-};//fn
+      case "email":
+        pattern = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/;
+        break;
+      case "tel":
+        pattern = /(\+3|0)6\({0,1}\d{2}\){0,1}[- ]{0,1}\d{3}[- ]{0,1}\d{3,4}/;
+        break;
+      default:
+        pattern = /^[a-z0-9]{2,}[ ,.'-]*[a-z0-9]+$/i;
+    }
+  } //if
+  return pattern;
+}; //fn
 
 Input.prototype.validate = function() {
-	var result = this.elem.checkValidity();
-	/*var pattern = new RegExp(this.pattern);
-	this.valid = pattern.test(input.elem.textContent);
-	var result = this.valid;*/
+  var result = this.elem.checkValidity();
+  /*var pattern = new RegExp(this.pattern);
+  this.valid = pattern.test(input.elem.textContent);
+  var result = this.valid;*/
   return result;
 }
 
 
 function createProgressBar() {
-  rawCardList.forEach(function(card,i){
+  rawCardList.forEach(function(card, i) {
     var li = document.createElement("li");
     li.textContent = card.name;
     progressBar.appendChild(li);
@@ -454,29 +448,30 @@ function createProgressBar() {
     if (i == 0) {
       span.style.background = "#3784e1";
     }
-    li.addEventListener("click",function(){
-        if(i != activeCard) {
-          if(i > activeCard) {
-            cardList[activeCard].validate();
-            var result = true, j = activeCard;
-            while (j < i) {
-              if (!cardList[j].valid) {
-                result = false;
-                var next = document.getElementById("cf-next");
-                next.click();
-                break;
-              }
-              j++;
+    li.addEventListener("click", function() {
+      if (i != activeCard) {
+        if (i > activeCard) {
+          cardList[activeCard].validate();
+          var result = true,
+            j = activeCard;
+          while (j < i) {
+            if (!cardList[j].valid) {
+              result = false;
+              var next = document.getElementById("cf-next");
+              next.click();
+              break;
             }
-            if(result) {
-              loadNextCard(i);
-            }
-
-          } else {
+            j++;
+          }
+          if (result) {
             loadNextCard(i);
           }
+
+        } else {
+          loadNextCard(i);
         }
-    },false);
+      }
+    }, false);
   });
 }
 
@@ -488,7 +483,7 @@ var cardIcon = document.getElementById("cf-icon");
 var inputSection = document.getElementById("cf-body");
 var progressBar = document.getElementById("cf-progressbar");
 
-var cardList = [];  //array of question cards
+var cardList = []; //array of question cards
 var activeCard = 0; //index of initial question card
 
 /* Tis few lines construct every card with
@@ -503,41 +498,42 @@ var card2 = new Card(rawCardList[2]);
 card1.copyCardTo(card2);
 var card3 = new Card(rawCardList[3]);
 
-card2.inputList[0].elem.addEventListener("change", function()	{
- 		card1.copyCardTo(card2);
-    card2.setDisabled(true);
-	},false);
+card2.inputList[0].elem.addEventListener("change", function() {
+  card1.copyCardTo(card2);
+  card2.setDisabled(true);
+}, false);
 
-card2.inputList[1].elem.addEventListener("change", function()	{
- 		card2.inputList.forEach(function(input) {
-	 		input.elem.value = "";
-      input.removePair().value = "";
- 		});
-		card2.setDisabled(false);
-	},false);
+card2.inputList[1].elem.addEventListener("change", function() {
+  card2.inputList.forEach(function(input) {
+    input.elem.value = "";
+    input.removePair().value = "";
+  });
+  card2.setDisabled(false);
+}, false);
 
 card3.inputList[0].elem.addEventListener("change", function() {
- 		card3.setDisabled(true);
-},false);
+  card3.setDisabled(true);
+}, false);
 
 card3.inputList[1].elem.addEventListener("change", function() {
- 		card3.setDisabled(false);
-},false);
-card3.inputList[5].elem.addEventListener("input", function(){
+  card3.setDisabled(false);
+}, false);
+
+card3.inputList[5].elem.addEventListener("input", function() {
   console.log("triggered");
   var elem = card3.inputList[5].elem;
   var m, d, y;
   switch (elem.value.length) {
-    case 2 :
+    case 2:
       m = elem.value;
       elem.value += "-";
       now = new Date();
       break;
-    case 7 :
+    case 7:
       y = elem.value.slice(-4);
-      m = elem.value.slice(0,2);
-      var exp = new Date(y,m);
-      if(now > exp){
+      m = elem.value.slice(0, 2);
+      var exp = new Date(y, m);
+      if (now > exp) {
         elem.setCustomValidity("Your card has been expired");
       } else {
         elem.setCustomValidity("");
@@ -545,4 +541,4 @@ card3.inputList[5].elem.addEventListener("input", function(){
       break;
   }
 
-})
+});
